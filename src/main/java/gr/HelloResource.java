b353f097ab7705195c8bcdbec0f72c9c37f6d3e5
gbourant <root@gbourant.com>
2024-06-26 10:52:32 +0300

    init reproducer

--- src/main/java/gr/HelloResource.java
+++ src/main/java/gr/HelloResource.java
@@ -1,5 +1,9 @@
 package gr;
 
+import io.quarkus.qute.Qute;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -8,9 +12,23 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/hello")
 public class HelloResource {
 
+    @Inject
+    Template login;
+
+    @Inject
+    Template validlogin;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    @Produces(MediaType.TEXT_HTML)
+    public String login() {
+        return login.instance().render();
     }
+
+    @GET
+    @Path("valid")
+    @Produces(MediaType.TEXT_HTML)
+    public String valid() {
+        return validlogin.instance().render();
+    }
+
 }
